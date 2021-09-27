e9d452e3f691f767f50317f55d805107f4d12021
alfystar <alfystar1701@gmail.com>
2021-09-27 16:00:08 +0200

    tesi wip

--- 02_ArduinoVersion/03_Code/pack-Controll.h
+++ 02_ArduinoVersion/03_Code/pack-Controll.h
@@ -1,52 +1,50 @@
 #ifndef PACK_H
 #define PACK_H
 #include <stdint.h>
-
+// #######################################################
+// ########        Companion to Arduino           ########
+// #######################################################
 struct newRef {
-  int16_t newRef;
+	int16_t newRef;
 } __attribute__((packed));
 
 struct setUpPackAsk {
-  int8_t padding;
+	int8_t padding;
 } __attribute__((packed));
 
 enum LinuxSendType : uint8_t { newRefType, askType };
 
-
 struct _packLinux2Ard {
-  LinuxSendType type;
-  union {
-    struct newRef ref;
-    struct setUpPackAsk ask;
-  };
+	LinuxSendType type;
+	union {
+		struct newRef ref;
+		struct setUpPackAsk ask;
+	};
 } __attribute__((packed));
 typedef struct _packLinux2Ard packLinux2Ard;
-
-//struct _packLinux2Ard {
-//  struct newRef ref;
-//} __attribute__((packed));
-//typedef struct _packLinux2Ard packLinux2Ard;
-
+// #######################################################
+// ########        Arduino to Companion           ########
+// #######################################################
 struct sample {
-  int16_t pwm;
-  int16_t V2_read;
-  int16_t Isense_read;
-  int16_t err;
+	int16_t pwm;
+	int16_t V2_read;
+	int16_t Isense_read;
+	int16_t err;
 } __attribute__((packed));
 
 struct setUpPack {
-  int16_t V2_mean;     // Adc read
-  int16_t Isense_mean; // Adc read
-  int16_t dt;          // Time in us (10^-6)
+	int16_t V2_mean;     // Adc read
+	int16_t Isense_mean; // Adc read
+	int16_t dt;          // Time in us (10^-6)
 } __attribute__((packed));
 enum ardSendType : uint8_t { sampleType, setUpPackType };
 
 struct _packArd2Linux {
-  ardSendType type;
-  union {
-    struct sample read;
-    struct setUpPack setUp;
-  };
+	ardSendType type;
+	union {
+		struct sample read;
+		struct setUpPack setUp;
+	};
 } __attribute__((packed));
 typedef struct _packArd2Linux packArd2Linux;
 
